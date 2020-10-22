cd4e713541d1669f86fc302fc378a8d1b99538d3
ghemsley <ghemsley@protonmail.ch>
2020-10-22 12:25:56 -0500

    commit tree 2affe927810fa862069bba77225dc055efe0f5dd

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,24 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf_names_array)
+  dwarf_names_array.each_with_index do |name, index|
+    puts "#{index + 1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls_array)
+  planeteer_calls_array.collect do |element|
+    "#{element.capitalize}!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(planeteer_calls_array)
+  planeteer_calls_array.any? do |element|
+    element.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(strings_array)
+  cheese_types = %w[cheddar gouda camembert]
+  strings_array.find do |string|
+    cheese_types.include?(string)
+  end
 end
